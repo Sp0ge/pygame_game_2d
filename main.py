a0bf12a74a1477bf771f99554426c38d3e8c3b27
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2021-11-16 13:25:20 +0300

    animation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pygame import sprite
 from pygame import display
 from pygame import image
 from game_config import *
+import this
 pygame.init()
 Game = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("THE BEST GAME EVER (Ver 0.1)")
@@ -18,6 +19,35 @@ player_stay = [pygame.image.load('sprite/stay/1.png'),
                pygame.image.load('sprite/stay/4.png'),
                pygame.image.load('sprite/stay/5.png'),
                pygame.image.load('sprite/stay/6.png'),]
+
+player_walk_right = [pygame.image.load('sprite/run_right/1.png'),
+               pygame.image.load('sprite/run_right/2.png'),
+               pygame.image.load('sprite/run_right/3.png'),
+               pygame.image.load('sprite/run_right/4.png'),
+               pygame.image.load('sprite/run_right/5.png'),
+               pygame.image.load('sprite/run_right/6.png'),]
+
+player_walk_left = [pygame.image.load('sprite/run_left/1.png'),
+               pygame.image.load('sprite/run_left/2.png'),
+               pygame.image.load('sprite/run_left/3.png'),
+               pygame.image.load('sprite/run_left/4.png'),
+               pygame.image.load('sprite/run_left/5.png'),
+               pygame.image.load('sprite/run_left/6.png'),]
+
+player_jump_right = [pygame.image.load('sprite/jump_right/1.png'),
+               pygame.image.load('sprite/jump_right/2.png'),
+               pygame.image.load('sprite/jump_rihgt/3.png'),
+               pygame.image.load('sprite/jump_right/4.png'),
+               pygame.image.load('sprite/jump_right/5.png'),
+               pygame.image.load('sprite/jump_right/6.png'),]
+
+player_jump_left = [pygame.image.load('sprite/jump_left/1.png'),
+               pygame.image.load('sprite/jump_left/2.png'),
+               pygame.image.load('sprite/jump_left/3.png'),
+               pygame.image.load('sprite/jump_left/4.png'),
+               pygame.image.load('sprite/jump_left/5.png'),
+               pygame.image.load('sprite/jump_left/6.png'),]
+
 
 
 
@@ -36,11 +66,11 @@ def draw():
         animCount = 0
 
     if walkleft:
-        image = pygame.transform.scale(player_stay[animCount//5], (PLAYER_WIDTH,PLAYER_HEIGHT))
+        image = pygame.transform.scale(player_walk_left[animCount//5], (PLAYER_WIDTH,PLAYER_HEIGHT))
         Game.blit(image,(PLAYER_X,PLAYER_Y))
         animCount += 1
-    if walkright:
-        image = pygame.transform.scale(player_stay[animCount//5], (PLAYER_WIDTH,PLAYER_HEIGHT))
+    elif walkright:
+        image = pygame.transform.scale(player_walk_right[animCount//5], (PLAYER_WIDTH,PLAYER_HEIGHT))
         Game.blit(image,(PLAYER_X,PLAYER_Y))
         animCount += 1
     else:
@@ -67,12 +97,10 @@ while True:
               
     elif keys[pygame.K_a] and PLAYER_X > 2:
         PLAYER_X -= PLAYER_SPEED
-        walkdown = True
-        walkup = False
+        walkleft = True
+        walkright = False
         
     else:
-        walkleft = False
-        walkright = False
         walkleft = False
         walkright = False
         amimCount = 0
