6aaa545df857653a6a2ec2e9de4234a9be5c7744
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2022-05-12 14:16:29 +0300

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -7,12 +7,31 @@ import os
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = "#FF6262"
+
+FINISH_WIDTH = 32
+FINISH_HEIGHT = 32
+FINISH_COLOR = "RED"
+
 ICON_DIR = os.path.dirname(__file__)
- 
+class Finish(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((FINISH_WIDTH, FINISH_HEIGHT))
+        self.image.fill(Color(FINISH_COLOR))
+        self.image = image.load("%s/blocks/finish.png" % ICON_DIR)
+        self.rect = Rect(x, y, FINISH_WIDTH, FINISH_HEIGHT)
+    
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
         self.image.fill(Color(PLATFORM_COLOR))
         self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+class Background(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.image = image.load("%s/blocks/background.png" % ICON_DIR)
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
